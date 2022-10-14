1b1e812a49c7455f29b102cac496e1d0127d851f
boykis82 <boykis82@gmail.com>
2022-10-14 14:57:13 +0900

    2022-10-14 2 json 역직렬화 시 없는 필드 무시

--- service/src/main/java/team/caltech/olmago/order/service/message/in/event/contract/ContractSubscriptionReceived.java
+++ service/src/main/java/team/caltech/olmago/order/service/message/in/event/contract/ContractSubscriptionReceived.java
@@ -1,5 +1,6 @@
 package team.caltech.olmago.order.service.message.in.event.contract;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,10 +9,10 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ContractSubscriptionReceived {
   private long contractId;
   private LocalDateTime eventOccurDtm;
   private long orderId;
   private String feeProductCode;
-  private List<String> productCodes;
 }
